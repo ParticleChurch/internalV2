c6015926b741ded51ca06b20628bf6f760db2f76
Graham <graham.donaldson17@gmail.com>
2021-11-28 00:00:49 -0800

    Added files
    Added file:
    - Globals.hpp/cpp
    - Hooks.hpp/cpp
    - Interfaces.hpp/cpp
    - Netvars.hpp/cpp

---
+++ internalV2/Globals.hpp
@@ -0,0 +1,58 @@
+#pragma once
+//#include "Entity.hpp"
+
+namespace G {
+	/*
+	// Menu Crap
+	extern bool MenuOpen;
+
+	// The injected DLL's module
+	extern HMODULE DLLModule;
+	extern bool KillDLL;
+	extern std::string CSGODirectory;
+	extern std::string CSGOExePath;
+
+	//General 
+	extern QAngle StartAngle;
+	extern QAngle EndAngle;
+	extern bool IsInGame;
+	extern bool FreezeTime;
+
+	//LocalPlayer
+	extern Entity* LocalPlayer;
+	extern bool LocalPlayerAlive;
+	extern int LocalPlayerIndex;
+	extern int LocalPlayerTeam;
+	extern Entity* LocalPlayerWeapon;
+	extern WeaponData* LocalPlayerWeaponData;
+	//maybe add flags and movetype?
+
+	//CreateMove
+	extern CUserCmd* cmd;
+	extern bool* pSendPacket;
+	extern float StartForwardMove;
+	extern float StartSideMove;
+
+	//Global Functions
+	extern void CM_Clamp();
+	extern void CM_Start(CUserCmd*, bool*);
+	extern void CM_MoveFixStart();
+	extern void CM_MoveFixEnd();
+	extern void CM_End();
+
+	// Pattern Scans / Convars
+	extern ConVar* UpdateRate;
+	extern ConVar* MaxUpdateRate;
+	extern ConVar* Interp;
+	extern ConVar* InterpRatio;
+	extern ConVar* MinInterpRatio;
+	extern ConVar* MaxInterpRatio;
+	extern ConVar* MaxUnlag;
+	extern DWORD AcceptMatchPattern;
+	extern DWORD pD3d9DevicePattern;
+	extern DWORD LoadSkyboxPattern;
+
+	extern void GUIInit();
+	extern void Init();
+	*/
+};
--- internalV2/Include.hpp
+++ internalV2/Include.hpp
@@ -1,5 +1,16 @@
+#pragma once
+
+#define _USE_MATH_DEFINES		 //to enable the use of mathmatecial constants   (like pi)
+
 #include <time.h>
 #include <Windows.h>
+
+#include "Utils/Utils.hpp"	//	includes all utils...
+#include "Interfaces.hpp"	//	includes all the interfaces..
+#include "Netvars.hpp"		//	for grabbing netvars
+
+// Semi-global variables for cheats
+#include "Globals.hpp"
 
 // Imgui
 #define IMGUI_DEFINE_MATH_OPERATORS
@@ -7,4 +18,5 @@
 #include "ImGui/imgui_impl_dx9.h"
 #include "ImGui/imgui_impl_win32.h"
 
-#include "Utils/Logger.hpp"
+// Hooks
+#include "Hooks.hpp"            //hooking ingame functions
