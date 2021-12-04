2785ebd8d6ba25ecd55da5c0721ada98e3b4666c
GabeMillikan <jj6403729@gmail.com>
2021-12-03 20:08:07 -0800

    Re-add new logger (+ fix it)

--- internalV2/Utils/Logger.cpp
+++ internalV2/Utils/Logger.cpp
@@ -1,54 +1,192 @@
-#include "../Include.hpp"
+#include "Logger.hpp"
 
-namespace L
+#include <string>
+#include <fstream>
+#include <iostream>
+
+#include <cstdint>
+#include <chrono>
+
+#include <filesystem>
+
+#include <Windows.h>
+
+L::OutputVerbosity previousVerbosity = L::OutputVerbosity::Debug;
+
+std::string logActiveFilePath;
+std::string logHistoryFilePath;
+std::ofstream outputFile;
+
+template <size_t max>
+inline size_t findNewline(const char* const& string)
 {
-	std::string FilePath = "a4g4.log";
-	std::ofstream File;
+	for (size_t i = 0; i < max; i++)
+		if (string[i] == '\n') return i;
+	return max;
 }
 
+void L::Log(const OutputVerbosity& verbosity, const char* txt, const char* end, const bool& flush, const bool& decorate)
+{
+	if constexpr (!LOGGING_ENABLED) return;
+	
+	previousVerbosity = verbosity; // so SameLine() can copy it
+	if (verbosity < OUTPUT_VERBOSITY) return;
+
+	static std::string decoratorString = "";
+	if (decorate)
+	{
+		decoratorString.clear();
+
+		const uint64_t unix = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+		decoratorString += std::to_string(unix);
+		decoratorString += " - [";
+
+		switch (verbosity)
+		{
+		case OutputVerbosity::Debug:
+			decoratorString += "DEBUG";
+			break;
+		case OutputVerbosity::Info:
+			decoratorString += "INFO";
+			break;
+		case OutputVerbosity::Crit:
+			decoratorString += "CRITICAL";
+			break;
+		default:
+			decoratorString += "UNKNOWN";
+			break;
+		}
+
+		decoratorString += "]: ";
+	}
+
+	if constexpr (OUTPUT_TO_CONSOLE)
+	{
+		if (decorate) std::cout << decoratorString;
+
+		std::cout << txt << end;
+
+		if (flush) std::cout << std::flush;
+	}
+
+	if constexpr (OUTPUT_TO_FILE)
+	{
+		if (!outputFile.is_open()) goto AFTER_FILE_OUTPUT;
+
+		if (decorate) // this is likely a new log entry
+		{
+			// so it's a good time to check if the file is full
+			if (outputFile.tellp() >= 1024 * OUTPUT_FILE_SIZE_KB)
+			{
+				// close and copy to history
+				outputFile.close();
+				std::filesystem::copy_file(logActiveFilePath, logHistoryFilePath, std::filesystem::copy_options::overwrite_existing);
+
+				// re-open and truncate
+				outputFile.open(logActiveFilePath, std::ios::out | std::ios::trunc);
+				if (!outputFile.is_open())
+				{
+					Crit("Failed to open logging file: ", "");
+					SameLine(logActiveFilePath.c_str());
+					goto AFTER_FILE_OUTPUT;
+				}
+			}
+
+			outputFile.write(decoratorString.c_str(), decoratorString.length());
+		}
+
+		outputFile.write(txt, strlen(txt));
+		outputFile.write(end, strlen(end));
+
+		if (flush) outputFile.flush();
+	
+	AFTER_FILE_OUTPUT:
+		{ /* have to put something afte a label*/ }
+	}
+}
+
+// forwards info on to L::Log
+void L::Debug(const char* txt, const char* end, const bool& flush)
+{
+	return L::Log(OutputVerbosity::Debug, txt, end, flush);
+}
+void L::Info(const char* txt, const char* end, const bool& flush)
+{
+	return L::Log(OutputVerbosity::Info, txt, end, flush);
+}
+void L::Crit(const char* txt, const char* end, const bool& flush)
+{
+	return L::Log(OutputVerbosity::Crit, txt, end, flush);
+}
+void L::SameLine(const char* txt, const char* end, const bool& flush)
+{
+	return L::Log(previousVerbosity, txt, end, flush, false);
+}
+
+// initalization
 void L::Init()
 {
-	File = std::ofstream(FilePath, std::ios::trunc);
-	if (!File.is_open())
+	// usually in release mode
+	if constexpr (LOGGING_ENABLED)
 	{
-		Log("!!!!!!!!ATTENTION!!!!!!!!");
-		Log("FAILED TO OPEN LOG FILE !!!!!!!!!");
+		// allocate console
+		if constexpr (OUTPUT_TO_CONSOLE)
+		{
+			AllocConsole();
+
+			FILE* fpstdin = stdin, * fpstdout = stdout, * fpstderr = stderr;
+			freopen_s(&fpstdin, "CONIN$", "r", stdin);
+			freopen_s(&fpstdout, "CONOUT$", "w", stdout);
+			freopen_s(&fpstderr, "CONOUT$", "w", stderr);
+		}
+
+		// open output file
+		if constexpr (OUTPUT_TO_FILE)
+		{
+			// create path to sibling of csgo.exe
+			char csgo_exe[MAX_PATH];
+			GetModuleFileName(NULL, csgo_exe, MAX_PATH);
+			std::string csgo_directory(csgo_exe, strlen(csgo_exe) - strlen("csgo.exe"));
+
+			logHistoryFilePath = csgo_directory + OUTPUT_FILE_NAME + "_history." + OUTPUT_FILE_EXTENSION;
+			logActiveFilePath = csgo_directory + OUTPUT_FILE_NAME + "_active." + OUTPUT_FILE_EXTENSION;
+
+			// truncate history file
+			outputFile.open(logHistoryFilePath, std::ios::out | std::ios::trunc);
+			if (outputFile.is_open()) outputFile.close();
+
+			// open and truncate the active file
+			outputFile.open(logActiveFilePath, std::ios::out | std::ios::trunc);
+			if (!outputFile.is_open())
+			{
+				Crit("Failed to open logging file: ", "");
+				SameLine(logActiveFilePath.c_str());
+			}
+		}
+
+		Info("Logger is constructed! This should be the first message.");
 	}
-	auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-
-	File << std::ctime(&time) << "\n";
-	File.flush();
 }
 
-void L::Log(const char* txt, const char* end, bool flush)
-{
-	File << txt << end;
-	if (flush)
-		File.flush();
-}
-
+// deconstruction
 void L::Free()
 {
-	File << "Logger Finished" << "\n";
+	if constexpr (LOGGING_ENABLED)
+	{
+		Info("Logger is deconstructing. This should be the last message.");
 
-	// close the file
-	File.close();
+		// free and destroy console
+		if constexpr (OUTPUT_TO_CONSOLE)
+		{
+			HWND con = GetConsoleWindow();
+			FreeConsole();
+			if (con) SendMessage(con, WM_CLOSE, NULL, NULL);
+		}
 
-	// remove the file, as we exited succesfully
-	//remove(FilePath.c_str());
-}
-
-
-void L::Info(const char* txt, const char* end, bool flush)
-{
-	File << txt << end;
-	if (flush)
-		File.flush();
-}
-
-void L::Debug(const char* txt, const char* end, bool flush)
-{
-	File << txt << end;
-	if (flush)
-		File.flush();
+		// close output file
+		if constexpr (OUTPUT_TO_FILE)
+		{
+			if (outputFile.is_open()) outputFile.close();
+		}
+	}
 }
--- internalV2/Utils/Logger.hpp
+++ internalV2/Utils/Logger.hpp
@@ -2,13 +2,39 @@
 
 namespace L
 {
-	extern std::string FilePath;
-	extern std::ofstream File;
+	// globally enable/disable logging
+	constexpr bool LOGGING_ENABLED = true;
 
-	extern void Init();
-	extern void Log(const char* txt, const char* end = "\n", bool flush = true);
-	extern void Free();
+	// console and or file output?
+	constexpr bool OUTPUT_TO_CONSOLE = false;
+	constexpr bool OUTPUT_TO_FILE = true;
+	constexpr const char* OUTPUT_FILE_NAME = "a4g4"; // will be placed as a sibling to csgo.exe
+	constexpr const char* OUTPUT_FILE_EXTENSION = "log"; // OUTPUT_FILE_NAME + "." + OUTPUT_FILE_EXTENSION
 
-	extern void Debug(const char* txt, const char* end = "\n", bool flush = true);
-	extern void Info(const char* txt, const char* end = "\n", bool flush = true);
+	// how much data should be kept in the output file? (it is a cyclic log)
+	// btw, this is an approximation, the actual amount of data will be slightly more
+	// since it rounds to the nearest log entry
+	// this is measured in kilobytes
+	constexpr size_t OUTPUT_FILE_SIZE_KB = 512;
+
+	enum class OutputVerbosity
+	{
+		Debug = 0,
+		Info,
+		Crit,
+	};
+
+	// everything above this importance will be outputted
+	constexpr OutputVerbosity OUTPUT_VERBOSITY = OutputVerbosity::Debug;
+	
+	// main functions
+	void Log(const OutputVerbosity& verbosity, const char* txt, const char* end = "\n", const bool& flush = true, const bool& decorate = true);
+	void Debug(const char* txt, const char* end = "\n", const bool& flush = true);
+	void Info(const char* txt, const char* end = "\n", const bool& flush = true);
+	void Crit(const char* txt, const char* end = "\n", const bool& flush = true);
+	void SameLine(const char* txt, const char* end = "\n", const bool& flush = true);
+
+	// initalization and deconstruction
+	void Init();
+	void Free();
 }
